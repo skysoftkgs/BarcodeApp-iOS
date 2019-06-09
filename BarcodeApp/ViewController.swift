c6ab13fcbb1484279b8619b5934335ec71818e1a
PingLi <pingli@pinglis-mac.local>
2019-06-08 18:48:03 -0700

    Initial Commit

---
+++ BarcodeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarcodeApp
+//
+//  Created by PingLi on 6/8/19.
+//  Copyright © 2019 PingLi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
